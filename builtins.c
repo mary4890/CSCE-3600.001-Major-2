fb848752951c6256f2b63cb9e8a76d15b2fe0ff4
PranavD88 <pranavdubey20248@gmail.com>
2025-07-21 17:07:42 -0500

    Update builtins.c
    temporaries

---
+++ builtins.c
@@ -0,0 +1,12 @@
+// Created by Pranav Dubey, Mary Adeeko, Hema Thallapareddy, Luke Marlin - CSCE 3600.001
+// Date: 7/21/2025
+// Description: File for built-in commands.
+
+#include "builtins.h"
+
+// Exit command
+
+// Cd Command
+
+// Myhistory command
+
