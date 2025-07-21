8a37908375743028c2aca8950f2762f3d70713ad
mary4890 <maryadeeko1@yahoo.com>
2025-07-21 18:04:50 -0500

    Update pipe.h

---
+++ pipe.h
@@ -0,0 +1,12 @@
+//pipe.h
+//Mary Adeeko, mya0051, Group 3
+//CSCE 3600.001
+//21 July 2025
+//Implements a way to handle pipelining of commands connected by pipe operator.
+
+#ifndef PIPE_H
+#define PIPE_H
+
+void handle_pipeline(char *line);
+
+#endif
