b3798fb3658e832d5746a14832a3e93331f6d611
Luke0731 <Luke.marlin294@gmail.com>
2025-07-21 22:22:37 -0500

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -1,12 +1,13 @@
 // Created by Pranav Dubey, Mary Adeeko, Hema Thallapareddy, Luke Marlin - CSCE 3600.001
 // Date: 7/21/2025
 // Description: File for built-in commands.
-
+#include "myhistory.h"
 #include "builtins.h"
 
 // Exit command
 
 // Cd Command
 
-// Myhistory command
+int builtin_myhistory(char *line, void (*reexecute)(char *)) {
+    return handle_myhistory(line, reexecute);
 
